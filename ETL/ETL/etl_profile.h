af1daf39b538adddd94f5e31d59bf9955b7b4718
jkm <medjk@comcast.net>
2014-04-26 17:52:49 -0700

    added etl_profile.h

---
+++ ETL/ETL/etl_profile.h
@@ -0,0 +1,33 @@
+/* ETL/etl_profile_.h.  Generated from etl_profile_.h.in by configure.  */
+/* ETL/etl_profile_.h.in.  Generated from configure.ac by autoheader.  */
+
+
+// We want to be autoconf/autoheader friendly, so
+// if the developer has already included an
+// autoheader-generated configuration script,
+// we can assume that the user knew what they
+// were doing and let them use their own config.h.
+//#ifndef ETL_TARNAME
+
+
+/* Define to the address where bug reports for this package should be sent. */
+#define ETL_BUGREPORT "http://www.synfig.org/issues/thebuggenie/synfig"
+
+/* Define to the full name of this package. */
+#define ETL_NAME "Extended Template Library"
+
+/* Define to the full name and version of this package. */
+#define ETL_STRING "Extended Template Library 0.04.18"
+
+/* Define to the one symbol short name of this package. */
+#define ETL_TARNAME "ETL"
+
+/* Define to the home page for this package. */
+#define ETL_URL ""
+
+/* Define to the version of this package. */
+#define ETL_VERSION "0.04.18"
+
+
+//#endif // ifndef ETL_TARNAME
+
