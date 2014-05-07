a050f954f6d6b84cc0611e0c537857551ef4c0db
jkm <medjk@comcast.net>
2014-05-06 23:37:49 -0700

    find freetype2 includes on Ubuntu

--- cmake/Modules/FindFreeType2.cmake
+++ cmake/Modules/FindFreeType2.cmake
@@ -18,9 +18,15 @@ pkg_check_modules(PC_FREETYPE2 xft)
 find_library(FreeType2_LIBRARY NAMES freetype libfreetype
     HINTS ${PC_FREETYPE2_LIBDIR} ${PC_FREETYPE2_LIBRARY_DIRS} ${FREETYPE2_PATH}/lib)
 
+# RPM
 find_path(FreeType2_INCLUDE_DIR freetype/freetype.h
     HINTS ${PC_FREETYPE2_INCLUDEDIR} ${PC_FREETYPE2_INCLUDE_DIRS} ${FREETYPE2_PATH}/include/freetype2
     PATH_SUFFIXES freetype)
+
+if(NOT FreeType2_INCLUDE_DIR)   
+    find_path(FreeType2_INCLUDE_DIR freetype.h
+        HINTS ${PC_FREETYPE2_INCLUDEDIR} ${PC_FREETYPE2_INCLUDE_DIRS} ${FREETYPE2_PATH}/include/freetype2)
+endif()
 
 set(FreeType2_PROCESS_INCLUDES FreeType2_INCLUDE_DIR)
 set(FreeType2_PROCESS_LIBS FreeType2_LIBRARY)
